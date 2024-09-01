8e3efa5269ed2fc74bf5aa476f996a752ed00f06
Yah-lee <yahleeperfectlove@gmail.com>
2024-09-01 23:48:03 +0700

    update

--- routes/bid.routes.js
+++ routes/bid.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/bid.controllers");
 
-router.post("/bid", controllers.createBid);
+const { bidValidator } = require("../middleware/validator");
+const validate = require("../middleware/validate");
+
+// router.post("/bid", controllers.createBid);
+router.post("/bid", bidValidator, validate, controllers.createBid);
+router.get("/bid", controllers.get_bids);
 
 module.exports = router;
