bc58e76179adb890144a25a7907b899172a8e802
Yah-lee <yahleeperfectlove@gmail.com>
2024-09-01 23:41:47 +0700

    update

--- controllers/bid.controllers.js
+++ controllers/bid.controllers.js
@@ -32,3 +32,30 @@ exports.get_bids = async (req, res) => {
     });
   }
 };
+
+exports.update_bid = async (req, res) => {
+  const { bid_id } = req.params;
+  const { product_id, bidder_id, bid_amount, bid_time, is_proxy_bid } =
+    req.body;
+  try {
+    const bid = await Bid.findOne({ where: { bid_id: bid_id } });
+    if (!bid) {
+      return res.status(404).json({ error: "Bid not found" });
+    }
+    const update_bid = {
+      product_id: product_id || bid.product_id,
+      bidder_id: bidder_id || bid.bidder_id,
+      bid_amount: bid_amount || bid.bid_amount,
+      bid_time: bid_time || bid.bid_time,
+      is_proxy_bid: is_proxy_bid || bid.is_proxy_bid,
+    };
+    await bid.update(update_bid);
+    return res.status(200).json({ bid: bid });
+  } catch (err) {
+    console.error("Error to update bid", err.message);
+    return res.status(500).json({
+      error: "Failed to update bid",
+      message: err.message,
+    });
+  }
+};
