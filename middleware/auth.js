68e1f8d8aeef2b2e4bad1a2c917c6db394acacd5
Yah-lee <yahleeperfectlove@gmail.com>
2024-09-11 18:05:04 +0700

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const authMiddleware = (req, res, next) => {
   if (!userId) {
     return res.status(400).json({ error: "You do not have permission" });
   }
-
   try {
     var decoded = jwt.verify(userId, "Secret@");
     req.user_id = decoded.username;
@@ -13,5 +12,4 @@ const authMiddleware = (req, res, next) => {
   }
   next();
 };
-
 module.exports = authMiddleware;
