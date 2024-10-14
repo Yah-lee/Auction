79277b8a62bc54965eb1a88cffe3c2fac570ea06
Yah-lee <yahleeperfectlove@gmail.com>
2024-10-14 17:15:10 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,26 +1,26 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("Auction", "root", "", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-  timezone: "+07:00",
-});
+// const { Sequelize } = require("sequelize");
+// const sequelize = new Sequelize("Auction", "root", "", {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   timezone: "+07:00",
+// });
+
+// // sequelize
+// //   .sync()
+// //   .then(() => {
+// //     console.log("Table created successfully!");
+// //   })
+// //   .catch((err) => {
+// //     console.log("Unable to create table:", err);
+// //   });
 
 // sequelize
-//   .sync()
+//   .authenticate()
 //   .then(() => {
-//     console.log("Table created successfully!");
+//     console.log("Database connection successful.");
 //   })
 //   .catch((err) => {
-//     console.log("Unable to create table:", err);
+//     console.error("Unable to connect to database:", err);
 //   });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connection successful.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to database:", err);
-  });
-
-module.exports = sequelize;
+// module.exports = sequelize;
