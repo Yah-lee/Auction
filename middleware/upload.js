45005af3bc204c9a330477e0a097df1255001ac4
Yah-lee <yahleeperfectlove@gmail.com>
2024-10-27 20:02:42 +0700

    update

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,35 +1,35 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+//     );
+//   },
+// });
 
-const upload = multer({  
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
-  fileFilter: function (req, file, cb) {
-    const fileTypes = /jpeg|jpg|png|gif|mp4|mkv/;
-    const extname = fileTypes.test(
-      path.extname(file.originalname).toLowerCase()
-    );
-    const mimetype = fileTypes.test(file.mimetype);
+// const upload = multer({  
+//   storage: storage,
+//   limits: { fileSize: 10 * 1024 * 1024 },
+//   fileFilter: function (req, file, cb) {
+//     const fileTypes = /jpeg|jpg|png|gif|mp4|mkv/;
+//     const extname = fileTypes.test(
+//       path.extname(file.originalname).toLowerCase()
+//     );
+//     const mimetype = fileTypes.test(file.mimetype);
 
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(new Error("Error: Only images and videos are allowed!"));
-    }
-  },
-});
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     } else {
+//       cb(new Error("Error: Only images and videos are allowed!"));
+//     }
+//   },
+// });
 
-module.exports = upload;
+// module.exports = upload;
