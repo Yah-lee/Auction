ad91acda0674833abc752829dd364ec5c868a2b1
Yah-lee <yahleeperfectlove@gmail.com>
2024-11-13 23:24:07 +0700

    update

--- controllers/chats.controllers.js
+++ controllers/chats.controllers.js
@@ -1,16 +1,16 @@
-// const { error } = require("console");
-// const chat = require("../models/chats.models");
+const { error } = require("console");
+const chat = require("../models/chats.models");
+exports.createChat = async (req, res) => {
+  const { sender_id, receiver_id, message } = req.body;
+  try {
+    const newChat = await chat.create({
+      sender_id,
+      receiver_id,
+      message,
+    });
+    return res.status(201).json({ newChat });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
 
-// exports.createChat = async (req, res) => {
-//   const { sender_id, receiver_id, message } = req.body;
-//   try {
-//     const newChat = await chat.create({
-//       sender_id,
-//       receiver_id,
-//       message,
-//     });
-//     return res.status(201).json({ newChat });
-//   } catch (err) {
-//     return res.status(500).json({ error: err.message });
-//   }
-// };
