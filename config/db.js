2a9b54998bbdccf2a5a86cb0c9b9c1573f0a4da0
Yah-lee <yahleeperfectlove@gmail.com>
2024-12-01 21:09:00 +0700

    upate

--- config/db.js
+++ config/db.js
@@ -5,14 +5,14 @@ const sequelize = new Sequelize("auction", "root", "", {
   timezone: "+07:00",
 });
 
-// sequelize
-//   .sync()
-//   .then(() => {
-//     console.log("Table created successfully!");
-//   })
-//   .catch((err) => {
-//     console.log("Unable to create table:", err);
-//   });
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Table created successfully!");
+  })
+  .catch((err) => {
+    console.log("Unable to create table:", err);
+  });
 
 sequelize
   .authenticate()
